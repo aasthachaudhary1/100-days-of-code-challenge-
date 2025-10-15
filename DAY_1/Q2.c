416e9a1f583e86927212b4350b48960e292bf217
aastha chaudhary <aasthach945@gmail.com>
2025-10-15 22:20:55 +0530

    Update Q2.c

--- DAY_1/Q2.c
+++ DAY_1/Q2.c
@@ -3,17 +3,11 @@
 int main() {
     int a, b;
     int sum, diff, product, quotient;
-
-    // Input two numbers
     scanf("%d %d", &a, &b);
-
-    // Calculations
     sum = a + b;
     diff = a - b;
     product = a * b;
-    quotient = a / b;  // Integer division
-
-    // Output
+    quotient = a / b;  
     printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d\n", sum, diff, product, quotient);
 
     return 0;
