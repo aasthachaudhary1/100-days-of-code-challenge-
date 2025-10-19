20dea8a102a50599e21b2f256370999a355bcfc4
aastha chaudhary <aasthach945@gmail.com>
2025-10-19 23:05:44 +0530

    Create Q2.c

---
+++ DAY_35/Q2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int k;
+    scanf("%d", &k);
+
+    k = k % n; 
+    int start = 0, end = n-1;
+    while(start < end){
+        int t = arr[start]; arr[start] = arr[end]; arr[end] = t;
+        start++; end--;
+    }
+    start = 0; end = k-1;
+    while(start < end){
+        int t = arr[start]; arr[start] = arr[end]; arr[end] = t;
+        start++; end--;
+    }
+    start = k; end = n-1;
+    while(start < end){
+        int t = arr[start]; arr[start] = arr[end]; arr[end] = t;
+        start++; end--;
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
