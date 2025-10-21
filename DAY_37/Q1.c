e0449c12bb58bc021d7dab7bddac07460f68b935
aastha chaudhary <aasthach945@gmail.com>
2025-10-21 14:23:39 +0530

    Create Q1.c

---
+++ DAY_37/Q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[100][100];
+    int rowSum[100];
+    for(int i = 0; i < rows; i++) {
+        rowSum[i] = 0; 
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            rowSum[i] += matrix[i][j];
+        }
+    }
+    for(int i = 0; i < rows; i++) {
+        printf("%d ", rowSum[i]);
+    }
+
+    return 0;
+}
