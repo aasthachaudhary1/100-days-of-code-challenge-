bd0d6eff598e1f0d5837f2435388abdb61043b8b
aastha chaudhary <aasthach945@gmail.com>
2025-10-25 23:40:26 +0530

    Create Q1.c

---
+++ DAY_44/Q1.c
@@ -0,0 +1,27 @@
+Q87: Count spaces, digits, and special characters in a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int spaces = 0, digits = 0, special = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        char ch = str[i];
+
+        if (ch == ' ') {
+            spaces++;
+        }
+        else if (ch >= '0' && ch <= '9') {
+            digits++;
+        }
+        else if (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))) {
+            special++;
+        }
+    }
+
+    printf("Spaces=%d, Digits=%d, Special=%d", spaces, digits, special);
+    return 0;
+}
