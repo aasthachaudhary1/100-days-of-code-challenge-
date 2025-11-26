6b4e8b1c4164b3c75a8c6e40cf3c71efb8681edd
aastha chaudhary <aasthach945@gmail.com>
2025-11-26 23:56:46 +0530

    Create Q126.c

---
+++ DAY_76/Q126.c
@@ -0,0 +1,23 @@
+Q126: Ask the user for a filename. Check if it exists by trying to open it in read mode. If the file pointer is NULL, print an error message; otherwise, read and display its content.
+
+#include <stdio.h>
+
+int main() {
+    char filename[100];
+    char line[1000];
+    printf("Enter filename: ");
+    scanf("%s", filename);
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error: File does not exist!\n");
+        return 1;
+    }
+
+    printf("File opened successfully.\n");
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+    fclose(fp);
+
+    return 0;
+}
