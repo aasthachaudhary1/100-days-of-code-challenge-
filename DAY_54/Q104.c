93972900a0fbb071145a80974c132d362dfbccc2
aastha chaudhary <aasthach945@gmail.com>
2025-11-26 20:01:57 +0530

    Create Q104.c

---
+++ DAY_54/Q104.c
@@ -0,0 +1,26 @@
+Q104: Write a Program to take a positive integer n as input, and find the pivot integer x such that the sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively. Print the pivot integer x. If no such integer exists, print -1. Assume that it is guaranteed that there will be at most one pivot integer for the given input.
+
+#include <stdio.h>
+#include <math.h>
+
+int findPivot(int n) {
+    int total = n * (n + 1) / 2;
+
+    int x = (int)sqrt(total);
+
+    if (x * x == total)
+        return x;
+
+    return -1;
+}
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    int pivot = findPivot(n);
+    printf("%d\n", pivot);
+
+    return 0;
+}
