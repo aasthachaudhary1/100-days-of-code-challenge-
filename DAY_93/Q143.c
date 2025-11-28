7360e2aa4bc215d70147c920f88b8c2472704c95
aastha chaudhary <aasthach945@gmail.com>
2025-11-28 17:22:37 +0530

    Create Q143.c

---
+++ DAY_93/Q143.c
@@ -0,0 +1,41 @@
+Q143: Find and print the student with the highest marks.
+
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    int n, i, topperIndex = 0;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    struct Student s[n];
+    for (i = 0; i < n; i++) {
+        printf("Enter details of student %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", s[i].name);
+
+        printf("Roll: ");
+        scanf("%d", &s[i].roll);
+
+        printf("Marks: ");
+        scanf("%d", &s[i].marks);
+
+        printf("\n");
+    }
+    for (i = 1; i < n; i++) {
+        if (s[i].marks > s[topperIndex].marks) {
+            topperIndex = i;
+        }
+    }
+    printf("Topper: %s (Marks: %d)\n",
+           s[topperIndex].name,
+           s[topperIndex].marks);
+
+    return 0;
+}
