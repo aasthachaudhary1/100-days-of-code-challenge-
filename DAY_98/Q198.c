da5d552d488321932f6ed04c887eac43264c54b8
aastha chaudhary <aasthach945@gmail.com>
2025-11-28 17:33:35 +0530

    Create Q198.c

---
+++ DAY_98/Q198.c
@@ -0,0 +1,34 @@
+Q148: Take two structs as input and check if they are identical.
+
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s1, s2;
+    printf("Enter details of Student 1:\n");
+    printf("Name: ");
+    scanf("%s", s1.name);
+    printf("Roll: ");
+    scanf("%d", &s1.roll);
+    printf("Marks: ");
+    scanf("%d", &s1.marks);
+    printf("\nEnter details of Student 2:\n");
+    printf("Name: ");
+    scanf("%s", s2.name);
+    printf("Roll: ");
+    scanf("%d", &s2.roll);
+    printf("Marks: ");
+    scanf("%d", &s2.marks);
+    if (strcmp(s1.name, s2.name) == 0 && s1.roll == s2.roll && s1.marks == s2.marks)
+        printf("\nSame\n");
+    else
+        printf("\nDifferent\n");
+
+    return 0;
+}
